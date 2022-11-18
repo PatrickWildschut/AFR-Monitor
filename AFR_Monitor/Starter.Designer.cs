3928bf3c9003231e99fcff4fff3cc47853d21890
DESKTOP-VVPH26D\patri <patrick.wildschut@hotmail.com>
2022-11-18 16:10:02 +0100

    Forgot to update the release date hahah
    Still version: 1.2.4

--- AFR_Monitor/Starter.Designer.cs
+++ AFR_Monitor/Starter.Designer.cs
@@ -415,7 +415,7 @@
             this.ReleaseDateLab.Name = "ReleaseDateLab";
             this.ReleaseDateLab.Size = new System.Drawing.Size(232, 13);
             this.ReleaseDateLab.TabIndex = 3;
-            this.ReleaseDateLab.Text = "Current Build Release Date: 11 November 2022";
+            this.ReleaseDateLab.Text = "Current Build Release Date: 18 November 2022";
             this.ReleaseDateLab.Click += new System.EventHandler(this.ReleaseDateLab_Click);
             // 
             // label3
